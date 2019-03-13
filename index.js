b9ae4f2d9791a5654db08368bae5bdadee533989
dahby <dstoll243@gmail.com>
2019-03-13 09:39:42 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,29 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const registerSelf = () => {
+  return fetch('http://guestbook.example.com/register', {
+    method: 'POST',
+    body: {
+      firstName: 'David',
+      registryMessage: 'I do not know'
+    }
+  })
+  .then(res => res.json())
+  .then(json => json.message)
+  .catch(err => ":(");
+}
+
+const errorSelf = () => {
+  return fetch('http://guestbook.example.com/register-error', {
+    method: 'POST',
+    body: {
+      firstName: 'David',
+      registryMessage: 'I do not know'
+    }
+  })
+  .then(res => res.json())
+  .then(json => json.message)
+  .catch(err => ":(");
+}
