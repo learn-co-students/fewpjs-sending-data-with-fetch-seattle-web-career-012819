e2936152058173ef502f7f1de885d75745374d92
github username <github email address>
2019-03-12 17:16:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const testVar = {}
+function registerSelf(){
+  return fetch("http://guestbook.example.com/register",{
+    "method": "POST",
+    "body": {
+      "firstName": "Joe",
+      "registryMessage": "Donely!"
+    }
+  }).then(function(response){
+    return response.json();
+  }).then(function(json){
+    return json.message;
+  })
+}
 
-function testFunc() {
-  return "hi"
+function errorSelf(){
+  return fetch("http://guestbook.example.com/register-error", {
+    "method": "POST",
+    "body": {
+      "firstName": "Pedru",
+      "registryMessage": "Speak Spanish plz"
+    }
+  }).then(function(response){
+    return response.json();
+  }).then(function(json){
+    return json.message;
+  }).catch(function(error){
+    console.log(error);
+    return ':(';
+  })
 }
