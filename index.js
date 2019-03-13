b93af3266ba99263584a27a7ed1912dc02b1fe14
Matthew Brophy <MBrophy@Matthews-MacBook-Air.local>
2019-03-13 09:51:27 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const testVar = {}
 
-function testFunc() {
-  return "hi"
+  function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+    "method": "POST",
+    "body": {
+      "firstName": "Matt",
+      "registryMessage": "Registered"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  });
+}
+
+let formData = {
+  "firstName": "Matt",
+  "registryMessage": "Look I can do stuff"
+};
+
+let configObj = {
+  method: "POST",
+  body: formData
+}
+
+function errorSelf() {
+  return fetch('http://guestbook.example.com/register-error', configObj)
+  .then(function(res) {
+    return res.json();
+  }).then(function(json) {
+    return json.messsage;
+  }).catch(function(x) {
+    return ":(";
+  })
 }
