28d50073d1a6df2123b0d5c6ce5a5221545d3403
Katie Carrel <katiecarrel@comcast.net>
2019-03-13 20:40:36 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const testVar = {}
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+    "method": "POST",
+    "body": {
+      "firstName": "Katie",
+      "registryMessage": "woo!"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  }).catch(function(x) {
+    return ":(";
+  });
+}
 
-function testFunc() {
-  return "hi"
+
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {
+    "method": "POST",
+    "body": {
+      "firstName": "Byron the Poodle",
+      "registryMessage": "Bite!"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  }).catch(function(x) {
+    return ":(";
+  });
 }
