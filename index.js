025c7b97563392917030514b1f38ad572883a8bf
jasonbasuil <basuilj@gmail.com>
2019-03-14 19:55:26 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const testVar = {}
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+    method: 'POST',
+    body: {
+      firstName: "Jason",
+      registryMessage: "Hello, this is my message to the registry"
+    }
+  })
+  .then(function(result) => {
+    return result.json();
+  }).then(function(json) {
+    return json.message;
+  });
+}
 
-function testFunc() {
-  return "hi"
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {
+    "method": "POST",
+    "body": {
+      "firstName": "Byron the Poodle",
+      "registryMessage": "Bite!"
+    }
+  }).then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    return json.message;
+  }).catch(function(x) {
+    return ":(";
+  });
 }
